9e3cd34db01e310e84bebd77a5c5d9797bbec0b3
Sagar Kanojia <isagarkanojia@gmail.com>
2022-09-03 17:03:37 +0530

    go fmt

--- main.go
+++ main.go
@@ -6,46 +6,46 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 
-	http.Handle("/",fileServer)
-	http.HandleFunc("/form",formHandler)
-	http.HandleFunc("/hello",helloHandler)
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8081\n")
 
-	if err := http.ListenAndServe(":8081",nil); err!=nil{
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func helloHandler(w http.ResponseWriter,r *http.Request){
-if r.URL.Path != "/hello"{
-	http.Error(w, "404 not found", http.StatusNotFound)
-	return
-}
-if r.Method!="GET"{
-	http.Error(w, "method is not supported", http.StatusNotFound)
-	return
-}
-
-fmt.Fprintf(w,"hello!")
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request){
-
-	if err := r.ParseForm(); err!=nil{
-		fmt.Fprintf(w, "ParseForm() err: %v",err)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w,"POST request successful")
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
 
-	name :=r.FormValue("name")
+	fmt.Fprintf(w, "hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+
+	name := r.FormValue("name")
 	address := r.FormValue("address")
 
-	fmt.Fprintf(w, " Name = %s\n",name)
-	fmt.Fprintf(w,"Address =%s",address)
+	fmt.Fprintf(w, " Name = %s\n", name)
+	fmt.Fprintf(w, "Address =%s", address)
 
 }
